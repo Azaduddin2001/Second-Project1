88ba8b16570ed8ea12184bf8ccd252617b00883b
Azaduddin2001 <mdazaduddin2001@gmail.com>
2025-03-12 11:22:20 +0600

    version 4

--- inheritance1.java
+++ inheritance1.java
@@ -3,5 +3,6 @@ public class inheritance1
     public static void main(String[] args)
     {
         System.out.println("FGUIFGGFGFF");
+        System.out.println("hello azad..........");
     }
 }
